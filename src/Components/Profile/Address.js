fc2df99a31333cb3dfc6c5850e16882c33d6d4cd
omobayode6 <omobayode93@gmail.com>
2022-06-06 20:55:15 +0100

    commiting first changes

---
+++ src/Components/Profile/Address.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Address = () => {
+  return(
+    <div>
+      <p>7, ogunyemi Street,</p>
+      <p>Alapere-Ketu, Lagos, Nigeria.</p>
+    </div>
+    
+  )
+}
+
+export default Address
---
+++ src/Components/Profile/ProfilePhoto.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const ProfilePhoto = () =>{
+  return(
+    <div>
+      <img src="./images/omobayode.png" alt="Profile" />
+    </div>
+  )
+
+}
+
+
+export default ProfilePhoto
